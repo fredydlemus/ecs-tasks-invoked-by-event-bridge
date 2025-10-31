790e4c9c338161be84f2ba18fa5dff6a501ce089
Fredy Flores Lemus <fredy@Fredys-MacBook-Pro.local>
2025-10-31 11:22:21 -0600

    create eventbridge rule

---
+++ eventbridge.tf
@@ -0,0 +1,16 @@
+resource "aws_cloudwatch_event_rule" "s3_object_created" {
+    name = "${local.name}-s3-object-created-rule"
+    description = "EventBridge rule to trigger ECS task on S3 object creation"
+
+    event_pattern = jsonencode({
+        "source": ["aws.s3"],
+        "detail-type": ["Object Created"],
+        "detail": {
+            "bucket": {
+                "name": [aws_s3_bucket.bucket.bucket]
+            }
+        }
+    })
+
+    tags = local.tags
+}
